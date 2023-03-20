c9960bd23fe7e85e3d5b5ffdef326736a7d34dfe
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-03-19 20:39:26 -0400

    Header component.

--- src/Pages/components/Header.tsx
+++ src/Pages/components/Header.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 // Components
 import Logo from "../../assets/Logo.png";
 
@@ -6,7 +5,7 @@ const Header = () => {
   return (
     <header className="bg-red-400 flex self-center items-center flex-col w-full p-4 gap-1">
       <img className="h-20" src={Logo} alt="Poke API logo" />
-      <span>FORMATTER</span>
+      <span className="Formatter">FORMATTER</span>
     </header>
   );
 };
