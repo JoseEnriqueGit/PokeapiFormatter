77dc9cb7f0e270cb5c3bc0baa8b5fa708366402b
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-03-19 23:11:49 -0400

    GridTemplateColumns and minWidth

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,19 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+        'HomeGrid': "repeat(auto-fit, minmax(300px, 1fr))",
+        // Simple 16 column grid
+        '16': 'repeat(16, minmax(0, 1fr))',
+
+        // Complex site-specific column configuration
+        'footer': '200px minmax(900px, 1fr) 100px',
+      },
+			minWidth: {
+				'80': '350px',
+			}
+		},
 	},
 	plugins: [],
 };
