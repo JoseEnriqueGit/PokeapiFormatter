0e449e482aa3943aa25182cb49ba1551fade7e30
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-03-19 23:14:46 -0400

    SearchIcon components.

---
+++ src/Pages/components/Icons/SearchIcon.tsx
@@ -0,0 +1,21 @@
+const SearchIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-search"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    strokeWidth={3}
+    stroke="#2c3e50"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    // {...props}
+  >
+    <path d="M0 0h24v24H0z" stroke="none" />
+    <circle cx={10} cy={10} r={7} />
+    <path d="m21 21-6-6" />
+  </svg>
+);
+
+export default SearchIcon;
