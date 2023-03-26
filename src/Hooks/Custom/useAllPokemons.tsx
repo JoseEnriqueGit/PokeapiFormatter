5fc8230ac9667fb3f9c4ef0f8cb3028e771d46d3
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-03-25 20:45:11 -0400

    custom hook useAllPokemons

---
+++ src/Hooks/Custom/useAllPokemons.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+type PokemonType = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+const useAllPokemons = () => {
+  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
+  const [pokemonsToFilter, setPokemonsToFilter] = useState<Pokemon[]>([]);
+
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100");
+      const results = response.data.results;
+      const pokemonDetails = await Promise.all(
+        results.map(async (result: any) => {
+          const details = await axios.get(result.url);
+          return { name: details.data.name, types: details.data.types.map((type: PokemonType) => type.type.name) };
+        })
+      );
+      setAllPokemons(pokemonDetails);
+      setPokemonsToFilter(pokemonDetails);
+    };
+    fetchPokemons();
+  }, []);
+  return [ allPokemons, pokemonsToFilter ]
+}
+
+export default useAllPokemons
