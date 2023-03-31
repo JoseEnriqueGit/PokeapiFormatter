e6003af933c7e5306143558c1422f6e00aa50e61
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-03-30 20:09:19 -0400

    custon height.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			gridTemplateColumns: {
-        'HomeGrid': "repeat(auto-fit, minmax(300px, 1fr))",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        HomeGrid: "repeat(auto-fit, minmax(300px, 1fr))",
         // Simple 16 column grid
-        '16': 'repeat(16, minmax(0, 1fr))',
+        16: "repeat(16, minmax(0, 1fr))",
 
         // Complex site-specific column configuration
-        'footer': '200px minmax(900px, 1fr) 100px',
+        footer: "200px minmax(900px, 1fr) 100px",
       },
-			minWidth: {
-				'80': '350px',
-			}
-		},
-	},
-	plugins: [],
+      minWidth: {
+        80: "350px",
+      },
+      height: {
+        Cust1: "62vh",
+      },
+    },
+  },
+  plugins: [],
 };
