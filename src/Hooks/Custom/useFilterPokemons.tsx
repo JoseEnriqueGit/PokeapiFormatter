3bee53b176f8aa11a2a645cbea8944e7d9416269
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-03-30 21:23:58 -0400

    Custom hook filter pokemons.

---
+++ src/Hooks/Custom/useFilterPokemons.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Fuse from "fuse.js";
+
+// https://pokeapi.co/api/v2/pokemon/?offset=20&limit=1281
+
+interface Pokemon {
+  url: any;
+  name: string;
+  types: string[];
+}
+
+type PokemonType = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+const options = {
+  threshold: 0.2,
+  keys: ["name"],
+};
+
+const useFilterPokemons = (toSearch: string, selectedType: string) => {
+  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
+  const [filteredPokemons, setFilteredPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=50"
+      );
+      const results = response.data.results;
+      const pokemonDetails = await Promise.all(
+        results.map(async (result: any) => {
+          const details = await axios.get(result.url);
+          return {
+            name: details.data.name,
+            url: result.url,
+            types: details.data.types.map(
+              (type: PokemonType) => type.type.name
+            ),
+          };
+        })
+      );
+      setAllPokemons(pokemonDetails);
+    };
+    fetchPokemons();
+  }, []);
+
+  useEffect(() => {
+    const fuse = new Fuse(allPokemons, options);
+    const filteredByName = toSearch
+      ? fuse.search(toSearch).map((result: { item: any }) => result.item)
+      : allPokemons;
+    const filteredByType =
+      selectedType !== "All"
+        ? filteredByName.filter((pokemon) =>
+            pokemon.types.includes(selectedType.toLowerCase())
+          )
+        : filteredByName;
+    setFilteredPokemons(filteredByType);
+  }, [toSearch, selectedType, allPokemons]);
+
+  return { filteredPokemons };
+};
+
+export default useFilterPokemons;
