ea04e9e59d89001054b9d86d67b2bff5950a59b2
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-04-03 12:42:29 -0400

    Barrels

--- src/Pages/components/index.ts
+++ src/Pages/components/index.ts
@@ -1,7 +1,7 @@
 export { default as Header } from './Header';
 export { default as SearchIcon } from './Icons/SearchIcon';
 export { default as PokemonCard } from './PokemonCard';
-export { default as Carrusel } from './Carrousel';
+export { default as PokemonCarousel } from './PokemonCarousel';
 export * from './SeachSection/components/Select';
 export * from './SeachSection/components';
 export * from './SeachSection';
---
+++ src/Types/index.ts
@@ -0,0 +1,1 @@
+export type { default as PokemonData } from './PokemonData';
